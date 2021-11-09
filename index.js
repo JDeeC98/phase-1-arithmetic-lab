24e6d491816940dc1973917a07adafc5e6521064
John David Carroll <johndavidcarroll@gmail.com>
2021-11-09 16:18:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31
+const num2 = 2
+let multiply = (num1*num2);
+multiply;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+random;
+
+let num3 = 19
+let num4 = 5
+let mod = (num3%num4)
+mod;
+
+let max = Math.max(2, 4, 8, 10, 20);
+max;
